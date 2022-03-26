a63bb1f823f0cbf6c13c689dc6dd608ab3ac231a
Kerem Dokumacı <kerem.dokumaci@yemeksepeti.com>
2022-03-26 19:31:08 +0300

    add: cli helper

---
+++ app/cli_helper.go
@@ -0,0 +1,18 @@
+package app
+
+import "fmt"
+
+type Color string
+
+const (
+	ColorBlack  Color = "\u001b[30m"
+	ColorRed          = "\u001b[31m"
+	ColorGreen        = "\u001b[32m"
+	ColorYellow       = "\u001b[33m"
+	ColorBlue         = "\u001b[34m"
+	ColorReset        = "\u001b[0m"
+)
+
+func colorizedText(color Color, message string) {
+	fmt.Println(string(color), message, string(ColorReset))
+}
